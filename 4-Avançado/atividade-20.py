ffc395daf47c8b22cad62a402427059369542e01
Ynot-DevCreator <antonyangelo250@gmail.com>
2025-07-16 15:05:14 -0300

    transformando em funções atividade 20

--- 4-Avançado/atividade-20.py
+++ 4-Avançado/atividade-20.py
@@ -5,22 +5,27 @@ arquivoTxt = "C:\\Users\Aluno\\Desktop\\Atividades python\\4-Avançado\\listaNum
 cont = 0
 soma = 0
 # ==============================================
-
-print('''=== Calculadora de Média de Números em Arquivo ===
+def leitorArquivo():
+    global cont, soma
+    print('''=== Calculadora de Média de Números em Arquivo ===
 
 =================================
 Lista de Números''')
-with open(arquivoTxt, "r", encoding="UTF-8") as arquivo:
-    linhas = arquivo.readlines()
-    for i in linhas:
-        i = float(i.strip())
-        print(f"{i}")
-        soma+=i
-        cont+=1
-    media = soma/cont
+    with open(arquivoTxt, "r", encoding="UTF-8") as arquivo:
+        linhas = arquivo.readlines()
+        for i in linhas:
+            i = float(i.strip())
+            print(f"{i}")
+            soma+=i
+            cont+=1
+    return soma/cont
+def mediaNumeros(media):
     print(f'''=================================
-          
+            
 =================================
 Quantidade de Números: {cont}
 Média dos Numeros da lista: {format(media, '.1f')}
 =================================''')
+
+media = leitorArquivo()
+mediaNumeros(media)
